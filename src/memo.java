c0c903eaa47ad2c4de0f63bffe8cbfdc3d778b31
haze0827 <167048997+haze0827@users.noreply.github.com>
2024-05-04 19:06:57 +0900

    잡코딩 24.5.4_01

---
+++ src/memo.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class memo {
+
+    //始まりは簡単にする方が良い！。
+    //たまにgiプシーとフールも勉強てましょ、、
+
+
+    public static void main(String[] args) {
+
+//
+//        int loop= 0;
+//
+//        while(true) {
+//            System.out.println("Looping: " + loop);
+//
+//            if (loop == 5) {
+//                break;
+//            }
+//
+//            loop++ ;
+//
+//
+//            System.out.println("-Running-");
+//        }
+//    }
+
+
+        scanner input = new Scanner(System.in);
+
+        System.out.println("엔터, 그 라인에 텍스트");
+    string line= input.nextline();
+
+        System.out.println("입력 값: " + line);
+
+
+}
